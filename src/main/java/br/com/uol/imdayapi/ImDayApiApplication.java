ac1d1a0d41da75a0092d11ba2a11a0fb9c829d76
Michael Pacheco <mdpgd95@gmail.com>
2021-09-14 17:47:29 -0300

    chore: generate project structure

---
+++ src/main/java/br/com/uol/imdayapi/ImDayApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.uol.imdayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImDayApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImDayApiApplication.class, args);
+    }
+
+}
