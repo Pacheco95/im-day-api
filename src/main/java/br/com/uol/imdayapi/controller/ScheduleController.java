e4d0520b115629cca5feb9bfc16bb7f60977c640
Michael Pacheco <mdpgd95@gmail.com>
2021-09-22 01:18:37 -0300

    feat: disable cors

--- src/main/java/br/com/uol/imdayapi/controller/ScheduleController.java
+++ src/main/java/br/com/uol/imdayapi/controller/ScheduleController.java
@@ -5,6 +5,7 @@ import br.com.uol.imdayapi.service.ScheduleService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ public class ScheduleController {
   private final ScheduleService scheduleService;
 
   @ApiOperation(value = "Returns a list of scheduled users from yesterday to 11 days ahead")
+  @CrossOrigin
   @GetMapping
   public List<Optional<User>> listRecentScheduledUsers() {
     return scheduleService.getRecentScheduledUsers();
